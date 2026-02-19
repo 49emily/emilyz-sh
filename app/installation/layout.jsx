fc715d478870c2c719073dbd70c1e0512498dfc5
Emily Zhang <emily49@stanford.edu>
2026-02-18 23:25:16 -0500

    metadata

--- app/installation/layout.jsx
+++ app/installation/layout.jsx
@@ -1,9 +1,9 @@
 export const metadata = {
   title: "installation",
-  description: "Installation, sculpture, and site-specific works by Emily Sihan Zhang.",
+  description: "installation, sculpture, and site-specificity",
   openGraph: {
     title: "emily sihan zhang > installation",
-    description: "Installation, sculpture, and site-specific works by Emily Sihan Zhang.",
+    description: "installation, sculpture, and site-specificity",
   },
 };
 
