7969db3f144e52cb92dab6d65ca54acee152222b
Emily Zhang <emily@crosbylegal.com>
2026-09-13 23:53:20 -0400

    fix

--- app/components/ProjectLink.jsx
+++ app/components/ProjectLink.jsx
@@ -6,7 +6,9 @@ const ProjectLink = ({ link, linkIndex, compact = false }) => {
       target="_blank"
       rel="noopener noreferrer"
       className={`font-semilight inline-flex items-center project-link rounded-full transition-colors ${
-        compact ? "px-2 py-0.5 text-xs" : "px-2 py-1 text-xs lg:text-sm"
+        compact
+          ? "px-2 py-0.5 text-xs lg:text-sm"
+          : "px-2 py-1 text-xs lg:text-sm"
       }`}
       onClick={(e) => e.stopPropagation()}
     >
